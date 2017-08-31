48127ccb28ded6bee99e13ae30916496e73773e5
Kaisma Penn-Titley <kptitley@gmail.com>
2017-08-30 21:52:53 -0400

    updated code?

---
+++ books.js
@@ -0,0 +1,14 @@
+//4. Create a Book Object and Put some in an Array
+// var BooksArray = function BooksArray() {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     BooksArray.push();
+// }
+
+
+
+var booksArray = ["girls who Code", "Stem GEMS", "Bible", "JavaScript for Dummies"];
+console.log(booksArray.length);
+booksArray.push("Stem Gems");
+Console.log(booksArray + " " + booksArray.length);
